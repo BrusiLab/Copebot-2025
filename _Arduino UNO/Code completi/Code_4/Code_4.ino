b7595cda9595bf7ffa7019fb36a5a7a3e417d0e9
BrusiLab <pietro.brusinelli@gmail.com>
2025-03-31 20:32:29 +0200

    modifica

---
+++ _Arduino UNO/Code completi/Code_4/Code_4.ino
@@ -0,0 +1,14 @@
+//include librerie
+#include "robot.h"
+
+Robot cope = Robot();  //creazione oggetto master (gestione motori)
+
+int vel = 1000; //rpm
+
+void setup() {
+  cope.set();
+}
+
+void loop() {
+  //le distanze sono da inserire in millimetri
+}
