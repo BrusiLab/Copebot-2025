5289ecaa76c47b0d4062b388d99a786ebd2cb582
Ongaretti <stefano.ongaretti@gmail.com>
2025-04-06 17:28:39 +0200

    codice da testare con servo (sbloccato)

--- _Arduino UNO/Code servo/Servo_v1/Servo_v1.ino
+++ _Arduino UNO/Code servo/Servo_v1/Servo_v1.ino
@@ -13,21 +13,23 @@
 */
 
 #include <Adafruit_PWMServoDriver.h>
+#include <Servo.h>
 #include <Wire.h>
 
 Adafruit_PWMServoDriver servo = Adafruit_PWMServoDriver();
+Servo servo_inclinatore //servo piccolo diverso (non attaccato a driver perché usa meno corrente)
 
 // posizione da 0 a 4095
 #define angolo_min 125
 #define angolo_mid 375 //90 gradi
 #define angolo_max 600
 
-#define ferma_servo 2
+#define ferma_servo       2 //interrompi corrente
 
-#define servo_inclinatore 0 //servo che inclina blocchi
-#define servo_L           1 //servo che raccoglie blocchi
-#define servo_ruota       2 //servo che sposta blocchi
-#define servo_bodyguard   3 //servo che contiene blocchi
+#define pin_inclinatore   4 //pin servo che inclina blocchi
+#define servo_L           0 //pos servo che raccoglie blocchi
+#define servo_ruota       1 //pos servo che sposta blocchi
+#define servo_bodyguard   2 //pos servo che contiene blocchi
 
 #define kfcaperto 5
 #define kfcchiuso 6
@@ -44,8 +46,11 @@ void interrompi() {
 void setup() {
 
   Serial.begin(9600);
+
   servo.begin();
   servo.setPWMFreq(60); //da 24 a 1600
+
+  servo_inclinatore.attach(pin_inclinatore);
 
   pinMode(kfcchiuso, INPUT);
   pinMode(ferma_servo, OUTPUT);
@@ -55,27 +60,27 @@ void setup() {
 
 void apri_L() {
 
-  digitalWrite(ferma_servo, LOW);
+  digitalWrite(ferma_servo, LOW); //attiva corrente
 
-  while (digitalRead(kfcaperto) == LOW && angolo_L < 4095) {
+  while (digitalRead(kfcaperto) == LOW && angolo_L < 4095) {  //finché non arrivi in fondo e non hai raggiunto max apertura
     servo.setPWM(servo_L, 0, angolo_L);
     angolo_L++;
   }
 
-  digitalWrite(ferma_servo, HIGH);
+  digitalWrite(ferma_servo, HIGH);  //blocca corrente
 }
 
-void chiudi_L(int posizione) {
+void chiudi_L(int posizione) {  
 
   digitalWrite(ferma_servo, LOW);
 
-  if (posizione == 0) {
+  if (posizione == 0) {   //posizione = 0 se fino in fondo
     fine = 0;
-  } else {
-    fine = 300; //non si chiude tutta --> blocchi sul cerchio esterno
+  } else {                //posizione = 1 se fino a metà cerchio
+    fine = 300;           //non si chiude tutta --> blocchi sul cerchio esterno
   }
 
-  while (digitalRead(kfcchiuso) == LOW && angolo_L > fine) {
+  while (digitalRead(kfcchiuso) == LOW && angolo_L > fine) {  //finché non arrivi a fine e non hai raggiunto min apertura
     servo.setPWM(servo_L, 0, angolo_L);
     angolo_L--;
   }
@@ -87,12 +92,18 @@ void loop() {
 
   digitalWrite(ferma_servo, LOW);
 
-  servo.setPWM(servo_inclinatore, 0, angolo_max);
+  servo.setPWM(servo_ruota, 0, angolo_max);
   delay(1000);
-  servo.setPWM(servo_inclinatore, 0, angolo_min);
+  servo.setPWM(servo_ruota, 0, angolo_min);
   delay(1000);
 
   digitalWrite(ferma_servo, HIGH);
+
+  delay(1000);
+
+  servo_inclinatore.write(0);
+  delay(500);
+  servo_inclinatore.write(100);
 
   delay(1000);
 
