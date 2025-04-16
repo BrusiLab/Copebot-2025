48c2c840f3e335bce2177ff5d6a543211ce70362
BrusiLab <pietro.brusinelli@gmail.com>
2025-04-16 14:23:43 +0200

    jubdsiuhfb

---
+++ _Arduino UNO/Code completi/code_3/code_3.ino
@@ -0,0 +1,62 @@
+//include librerie
+#include "twoSteppers.h"  // Stepper ruote
+#include "lidar.h"
+#include <Adafruit_PWMServoDriver.h>
+#include <Servo.h>
+#include <Wire.h>
+
+// Oggetti
+Robot robot = Robot();                                      // Dichiaro oggetto robot (gestione stepper)
+Lidar lidar = Lidar();                                      // Dichiaro oggetto Lidar
+Adafruit_PWMServoDriver servo = Adafruit_PWMServoDriver();  // Dichiaro oggetto driver
+
+// Prototipi funzioni comunicazione
+String ricevi();
+void invia(String line);
+void blink(int pin);
+
+// Prototipi funzioni servo
+void servo_set();
+void apri_L();
+void chiudi_L(int posizione = 1);
+void apri_leva();
+void chiudi_leva();
+
+// Prototipi funzioni percorso
+void inizio();
+void avanza();
+void raccogli();
+void scarica();
+void ritorna();
+
+// PINS
+#define ferma_servo 7  // Interrompi corrente
+#define kfcaperto 9    // Fine corsa per L aperta
+#define kfcchiuso 10   // Fine corsa per L chiusa
+#define LED 8          // LED pin
+
+
+void setup() {
+  Serial.begin(9600);
+  // Inizializzazione comunicazione I2C gyro e servo
+  robot.set();
+  servo_set();
+
+  // Dichiarazione pins
+  pinMode(kfcchiuso, INPUT);
+  pinMode(kfcaperto, INPUT);
+  pinMode(ferma_servo, OUTPUT);
+  pinMode(LED, OUTPUT);
+
+  while (!Serial) {}
+}
+
+
+void loop() {
+  delay(1000);
+  raccogli();
+  /*while (true) {
+    avanza();
+  }*/
+  //digitalWrite(LED, HIGH);
+}
---
+++ _Arduino UNO/Code completi/code_3/funzioniPercorso.ino
@@ -0,0 +1,55 @@
+#define velocitaAvanza 400
+#define distanzaAvanza 500  //mm
+unsigned int avanzaCM = 0;
+
+void inizio() {
+}
+
+
+void avanza() {
+  robot.vai(10, velocitaAvanza, "avanti", "off");
+  int misuraCM = lidar.misura();
+
+  if (misuraCM < 4) {
+    robot.vai(40, velocitaAvanza, "avanti", "off");
+
+    while (true) {
+
+      invia("rileva");
+      String colore = ricevi();
+
+      if (colore == "rosso" || colore == "giallo") {
+        digitalWrite(LED, HIGH);
+        invia("nothing");
+        raccogli();
+        digitalWrite(LED, LOW);
+        delay(50);
+      }
+
+      break;
+    }
+
+    robot.vai(50, velocitaAvanza, "avanti", "off");
+    misuraCM = lidar.misura();
+  }
+}
+
+
+void raccogli() {
+  delay(100);
+  robot.vai(100, velocitaAvanza, "avanti", "off");
+  apri_leva();
+  delay(500);
+  apri_L();
+  delay(500);
+  chiudi_leva();
+  delay(500);
+  chiudi_L(0);
+  delay(500);
+}
+
+void scarica() {
+}
+
+void ritorna() {
+}
